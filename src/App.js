af9ba81a84f697d17a9b08d1469add90397b394b
NabilRaiyan <rraiyan77@gmail.com>
2024-05-19 21:36:04 +0600

    shallow merge

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     super();
 
     this.state = {
-      name: "Jhon",
+      name: {firstname: "Raiyan", lastname:"Al Sultan",},
       age: 24,
     }
   }
@@ -18,9 +18,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <p>
-          Hello {this.state.name} and I am {this.state.age} years old.
+          Hello {this.state.name.firstname} {this.state.name.lastname} and I am {this.state.age} years old.
           </p>
-          <button>Change Name</button>
+          <button onClick={()=>{
+            this.setState({name: {firstname: "Nabil", lastname:"Abdullah"}, age:34});
+          }}>Change Name</button>
         </header>
       </div>
     );
