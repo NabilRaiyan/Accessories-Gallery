f202bea533984d96ca4c78bab6dd4d4f87ef30d6
NabilRaiyan <rraiyan77@gmail.com>
2024-05-20 20:46:50 +0600

    adding function

--- src/App.js
+++ src/App.js
@@ -8,29 +8,31 @@ class App extends Component {
     super();
 
     this.state = {
-      name: {firstname: "Raiyan", lastname:"Al Sultan",},
-      age: 24,
-    }
+      // name: {firstname: "Raiyan", lastname:"Al Sultan",},
+      // age: 24,
+      accessories: [
+        {
+          name: "watch",
+        },
+        {
+          name: "bracelet",
+        },
+        {
+          name: "ring",
+        },
+    ]
+    };
   }
 
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-          Hello {this.state.name.firstname} {this.state.name.lastname} and I am {this.state.age} years old.
-          </p>
-          <button onClick={()=>{
-            // this.setState({name: {firstname: "Nabil", lastname:"Abdullah"}, age:34});
-            this.setState(()=>{
-              return { // this is the shawllo merge function
-                name: {firstname: "Nabil", lastname:"Abdullah", age:34},
-              }
-            }, ()=>{ // this is a callback function
-              console.log(this.state);
-            });
-          }}>Change Name</button>
-        </header>
+        <h1>Accessories</h1>
+        {
+          this.state.accessories.map((accessory)=>{
+            return <h1>{accessory.name}</h1>
+          })
+        }
       </div>
     );
   }
